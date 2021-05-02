5eb5aa1a8bc9334ed703809b1adda42815325415
Your Name <you@example.com>
2021-05-02 18:59:59 +0200

    add

---
+++ PhoneBook/src/NamePhone.java
@@ -0,0 +1,8 @@
+public class NamePhone {
+    public static void main ( String[] args ) {
+
+        NumberType numberType = NumberType.HOME;
+
+        System.out.println (numberType.getName () );
+    }
+}
